acb536d97d7b5f1bc75ab19f92c3456197e056cd
Dougal Scott <Dougal.Scott@gmail.com>
2016-09-22 15:23:29 +1000

    Make work

--- proc.c
+++ proc.c
@@ -1,39 +1,33 @@
 /*
  * texproc: embedded command preprocessor for TeX and LaTeX
- * (c) 1992 Dougal Scott
+ * (c) 2016 Dougal Scott
  * Any comments, criticisms, patches to 
- * Dougal.Scott@FCIT.monash.edu.au
+ * Dougal.Scott@gmail.com
  *
  * Convert LaTeX:
- * ....
+ * ...
  * %# gnuplot
  * plot sin, cos, and tan
  * %#
- * ....
+ * ...
  *
  * to 
  *
- * ....
+ * ...
  * \begin{picture}
  * \lotsadots
  * \end{picture}
- * ....
+ * ...
  */
-
-#define TRACE(x)	/* x */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <strings.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <errno.h>
-#include <limits.h>
 
 int process(char *);
 
 extern int errno;
-FILE *tmp;
+FILE *tmpfh;
 
 int main(int argc, char **argv)
 {
@@ -44,50 +38,62 @@ int main(int argc, char **argv)
             process(buff);
         }
         else {
-            fprintf(stdout, "%s\n", buff);
+            fprintf(stdout, "%s", buff);
         }
     }
     return(0);
 }
 
-int process(char *buff)
+int runprogram(char *progname, char *tmpname)
 {
-    char 	tmpname[80],		/* Name of tmp file */
-            progname[80],		/* Name of program to execute */	
-            cmdline[80],		/* What to feed to popen */
-            outbuff[BUFSIZ];	/* What to take output of prgram from */
     FILE	*p;
+    char cmdline[BUFSIZ];
+    char outbuff[BUFSIZ];
 
-    strcpy(tmpname, "/tmp/PrcXXXXXX");
-    mktemp(tmpname);
-    if((tmp=fopen(tmpname, "w"))==NULL) {
+    progname[strcspn(progname, "\n")] = 0;
+    sprintf(cmdline, "%s %s", progname, tmpname);
+    fprintf(stderr, "%s %s\n", progname, tmpname);
+    if((p=popen(cmdline, "r"))==NULL) {
+        fprintf(stderr, "Could not open pipe to %s\n", cmdline);
+        exit(-1);
+    }
+    while(fgets(outbuff, sizeof(outbuff), p)!=NULL) {
+        fprintf(stdout, "%s", outbuff);
+    }
+    pclose(p);
+    // unlink(tmpname);
+    return(0);
+}
+
+FILE *open_temp(char *tmpname) {
+    if((tmpfh=fopen(tmpname, "w"))==NULL) {
         fprintf(stderr, "Could not open %s for writing\n", tmpname);
         fprintf(stderr, "Program aborting\n");
         exit(-1);
         }
-    TRACE(fprintf(stderr, "Saving to tmp file %s\n", tmpname));
+    return tmpfh;
+}
+
+int process(char *buff)
+{
+    char tmpbuf[BUFSIZ];
+    char progname[BUFSIZ];
+    char tmpname[BUFSIZ];
+
+    strcpy(tmpname, "/tmp/PrcXXXXXX");
+    mktemp(tmpname);
+
+    tmpfh = open_temp(tmpname);
     strcpy(progname, &buff[3]);
     fprintf(stdout, "%% Including output from %s\n", progname);
-    /* Put buffer to file for executing */
-    while(fgets(buff, sizeof(buff), stdin)!=NULL) {
-        if(buff[0]=='%' && buff[1]=='#') {
-            fclose(tmp);
-            sprintf(cmdline, "%s %s", progname, tmpname);
-            fprintf(stderr, "%s\n", progname);
-            if((p=popen(cmdline, "r"))==NULL) {
-                fprintf(stderr, "Could not open pipe to %s\n", cmdline);
-                exit(-1);
-			}
-            while(fgets(outbuff, BUFSIZ, p)!=NULL) {
-                fprintf(stdout, "%s", outbuff);
-            }
-            pclose(p);
-            unlink(tmpname);
+    while(fgets(tmpbuf, sizeof(tmpbuf), stdin) != NULL) {
+        if(tmpbuf[0]=='%' && tmpbuf[1]=='#') {
+            fclose(tmpfh);
+            runprogram(progname, tmpname);
             return(0);
         }
         else {
-            TRACE(fprintf(stderr, "%s\n", buff));
-            fprintf(tmp, "%s\n", buff);
+            fprintf(tmpfh, "%s", tmpbuf);
         }
     }
     return(0);
